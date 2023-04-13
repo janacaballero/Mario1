6130bb3878371186cdcb6e57752b36e157260601
llucmacias <128025994+llucmacias@users.noreply.github.com>
2023-04-13 08:53:28 +0200

    cody

---
+++ KinematicBody2D.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+#	if is_on_floor():
+#			if Input.is_action_pressed("dreta"):
+##				Desplacament += Vector2 (5,0)
+#				Anim_Sprite.play("run")
+##				actualitza_direccio(velocity)
+#			if Input.is_action_pressed("enfadat"):
+#				Anim_Sprite.play("nova")
+#			if Input.is_action_pressed("esquerra"):
+##				Desplacament += Vector2 (-5,0) 
+#				Anim_Sprite.play("run")
+#	#			actualitza_direccio(velocity)
+#			if Input.is_action_pressed("amunt"):
+#				Anim_Sprite.play("jump")
+#				Moviment.y= -Salt
+#				get_node("salt").play()
+#				if Input.is_action_pressed("amunt") and (Input.is_action_pressed("dreta") or Input.is_action_pressed("esquerra")):
+#					Anim_Sprite.play("jump")
+#					Moviment.y= -Salt
+#	#else: 
+#		#	if Input.is_action_pressed("dreta") or Input.is_action_pressed("esquerra"):
+##				Anim_Sprite.play("jump")
+##		if Input.is_action_pressed("dreta"):
+##				Desplacament += Vector2 (2,0)
+##		if Input.is_action_pressed("esquerra"):
+##				Desplacament += Vector2 (-2,0)
+###				print("Esquerra!")
+##		if Input.is_action_pressed("descans"):
+##			Anim_Sprite.play("descans")
+##
+##		if Input.is_action_pressed("amunt") == false and Input.is_action_pressed("dreta") == false and Input.is_action_pressed("esquerra") == false and Input.is_action_pressed("enfadat") == false and is_on_floor():
+##			Anim_Sprite.play("idle")
+##			Desplacament = Vector2.ZERO
+###		print(Desplacament)
+##		Moviment += Desplacament.normalized() * Velocitat
+##		Moviment += Gravetat * delta
+##		actualitza_direccio(Moviment)
+##	#	velocity = Moviment
+##		move_and_slide(Moviment, Vector2.UP)
+##		Desplacament.x = 0
+##
+##
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
