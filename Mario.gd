b3391d982961f54c670747e3f2690fa1d7534834
janacaballero <128026241+janacaballero@users.noreply.github.com>
2023-04-17 09:50:32 +0200

    moviment

---
+++ Mario.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+var velocitat := 1000
+var moviment := Vector2.ZERO
+var gravetat := Vector2.DOWN *400
+var direccio := Vector2.ZERO
+var salt := 300
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("saltar") and is_on_floor():
+		moviment.y = -salt
+	if Input.is_action_pressed("endavant"):
+		moviment.x = 300
+	if Input.is_action_pressed("endarrere"):
+		moviment.x = -300
+	
+#	move_and_slide(direccio.normalized() * velocitat)
+	moviment += gravetat * delta
+	
+	moviment = move_and_slide(moviment, Vector2.UP)
+	
