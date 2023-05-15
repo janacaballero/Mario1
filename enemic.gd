04ede0649ccc12dd0f6b5aa7a436f27e33777682
rruizsape <128025962+ainaboncat@users.noreply.github.com>
2023-05-15 09:59:55 +0200

    area interrogant

--- enemic.gd
+++ enemic.gd
@@ -12,16 +12,14 @@ func _process(delta):
 	print(moviment)
 	moviment = move_and_slide(moviment, Vector2.UP)
 
-
-
 func _on_Area2DDreta_body_entered(body):
 	if body.is_in_group("terra"):
 		moviment.x = -200
 	if body.is_in_group("mario"):
 		pass
 
-
-
 func _on_Area2DEsquerra_body_entered(body):
 	if body.is_in_group("terra"):
 		moviment.x = 200
+	if body.is_in_group("mario"):
+		pass
