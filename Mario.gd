10c1d15f104d19b018c10f3184da9caf0bdd2262
ainaboncat <128025962+ainaboncat@users.noreply.github.com>
2023-05-17 15:39:48 +0200

    camera

--- Mario.gd
+++ Mario.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 var velocitat := 1000
 var moviment := Vector2.ZERO
-var gravetat := Vector2.DOWN *600
+var gravetat := Vector2.DOWN *650
 var direccio := Vector2.ZERO
-var salt := 320
+var salt := 350
 
 func _process(delta):
 	if Input.is_action_pressed("saltar") and is_on_floor():
